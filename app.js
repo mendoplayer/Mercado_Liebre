b1495a9f4cd53337192723c8a023affeb5a94f4b
mendoplayer <fernandocampos19888@gmail.com>
2021-11-29 19:45:43 -0300

    Agrague app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const app = express();
+const path = require ('path');
+
+app.set ('port', 3000);
+
+app.listen (app.get('port'), () => console.log('Servidor funcionaddo'));
+
+app.get ('/',(req,res) => res.sendFile(path.resolve(__dirname,'./views/home.html')));
+
+
+app.use (express.static (path.resolve (__dirname, './public')));
+
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
+
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
