8acfebaf15b96fcdcd524a6d2fd8490e6c1e7404
mendoplayer <fernandocampos19888@gmail.com>
2021-12-07 18:56:15 -0300

    Modifico para subir a heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 const path = require ('path');
 
-app.set ('port', 3000);
+app.set ('port', process.env.PORT || 3000);
 
 app.listen (app.get('port'), () => console.log('Servidor funcionaddo'));
 
